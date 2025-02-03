f43b1a14795537ef72cf8bfaff8116125dd21fda
James John McGuire <jamesjohnmcguire@gmail.com>
2025-02-03 16:54:32 +0900

    Keep config.h

---
+++ config.h
@@ -0,0 +1,17 @@
+#define HAVE_ROUND 1
+#define HAVE_LRINTF 1
+/* #undef HAVE_AV_PACKET_UNREF */
+/* #undef HAVE_AV_FRAME_ALLOC */
+/* #undef HAVE_AV_FRAME_FREE */
+
+#define TESTS_DIR "tests/"
+
+/* #undef USE_SWRESAMPLE */
+/* #undef USE_AVRESAMPLE */
+#define USE_INTERNAL_AVRESAMPLE 1
+
+/* #undef USE_AVFFT */
+/* #undef USE_FFTW3 */
+/* #undef USE_FFTW3F */
+/* #undef USE_VDSP */
+#define USE_KISSFFT 1
